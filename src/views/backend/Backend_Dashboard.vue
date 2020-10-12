660359392dbf51af54348e55e7f7b1ffd72c296e
Emily Chen <wanchichen88@gmail.com>
2020-10-12 17:08:23 +0800

    修改字型

--- src/views/backend/Backend_Dashboard.vue
+++ src/views/backend/Backend_Dashboard.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
     <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-      <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3 chewy" href="#">a little</a>
+      <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3 passeroOne" href="#">a little</a>
       <button
         class="navbar-toggler position-absolute d-md-none collapsed"
         type="button" data-toggle="collapse"
