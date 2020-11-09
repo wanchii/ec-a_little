75a944876b9f921c37cf7a79cd407790067f1e7d
Emily Chen <wanchichen88@gmail.com>
2020-11-09 16:47:19 +0800

    新增layout資料夾

---
+++ src/views/layout/LayoutA.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+  <div class="content">
+    <Navbar />
+    <router-view></router-view>
+  </div>
+    <Footer />
+</div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue';
+import Footer from '@/components/Footer.vue';
+
+export default {
+  data() {
+    return {
+    };
+  },
+  components: {
+    Navbar,
+    Footer,
+  },
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/views/layout/LayoutB.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+  <div class="content">
+    <NavbarSimple />
+    <router-view></router-view>
+  </div>
+    <FooterSimple />
+</div>
+</template>
+
+<script>
+import NavbarSimple from '@/components/Navbar-simple.vue';
+import FooterSimple from '@/components/Footer-simple.vue';
+
+export default {
+  data() {
+    return {
+    };
+  },
+  components: {
+    NavbarSimple,
+    FooterSimple,
+  },
+};
+</script>
+
+<style>
+
+</style>
